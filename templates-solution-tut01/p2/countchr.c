33e7c250b7f011f88cbef8078104be4de5bac030
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-01-11 16:10:26 +0000

    Update countchr.c

--- templates-solution-tut01/p2/countchr.c
+++ templates-solution-tut01/p2/countchr.c
@@ -1,11 +1,12 @@
 #include "countchr.h"
 
 int countchr(char *string, char c) {
+
 	// Add code here.
 	int count = 0;
 
 	while (*string != '\0'){
-		if (*(string = c)) {
+		if (*string = c) {
 			count++;
 
 		};
