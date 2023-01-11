0570ec9df2095a43a84c18d2dba98ea53edf492a
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-01-11 18:49:19 +0000

    Update main.c

--- P_assignment1/p2/main.c
+++ P_assignment1/p2/main.c
@@ -5,7 +5,7 @@ int main()
 {
     test_start("parseint.c");
 
-    test_equals_int(parseDecimalChar('10'), -1);
+    test_equals_int(parseDecimalChar('g'), -1);
     test_equals_int(parseInt("010"), 8);
     test_equals_int(parseInt("10"), 10);
     test_equals_int(parseInt("?"), -1);
