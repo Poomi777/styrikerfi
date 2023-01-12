78b233f6af908fb621547fd554dee77403964d34
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-01-12 17:39:59 +0000

    Update main.c

--- P_assignment1/p2/main.c
+++ P_assignment1/p2/main.c
@@ -9,8 +9,6 @@ int main()
     test_equals_int(parseInt("010"), 8);
     test_equals_int(parseInt("10"), 10);
     test_equals_int(parseInt("?"), -1);
-    test_equals_int(parseInt("8"), 8);
-    test_equals_int(parseInt("6"), 6);
 
     return test_end();
 }
