ba30b7425b7e0c4bf2b16e4f33d24ebc88a9f0f1
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-01-12 17:37:43 +0000

    Update parseint.c

--- P_assignment1/p2/parseint.c
+++ P_assignment1/p2/parseint.c
@@ -39,7 +39,7 @@ int parseInt(char *string)
             string++;
             dec_converter = dec_converter + *string;
         }
-        return dec_converter - ('0'<<1);
+        return dec_converter >> 4;
     }
 
     else if ( (*string == '0') ){
