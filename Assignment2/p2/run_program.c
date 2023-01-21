33af4f215f84db23dbae1d00f1bd10e127c80448
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-01-21 23:41:39 +0000

    Update run_program.c

--- Assignment2/p2/run_program.c
+++ Assignment2/p2/run_program.c
@@ -1,5 +1,10 @@
 #include "run_program.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/*remember to check out https://man7.org/linux/man-pages/man3/exec.3.html*/
+
 #define ERROR_CODE 127
 
 int run_program(char *file_path, char *argv[])
@@ -7,6 +12,34 @@ int run_program(char *file_path, char *argv[])
     (void) file_path;
     (void) argv;
 
+    int command = fork();
+    int status;
+
+    if (file_path == NULL) {
+        return ERROR_CODE;
+    }
+
+    if (command == 0) {
+        if (argv == NULL) {
+            execve(file_path, file_path, NULL);
+        }
+        exit(ERROR_CODE);
+    }
+
+    else if (command > 0) {
+        waitpid(command, &status, 0);
+
+        if (WIFEXITED(status)) {
+            return WEXITSTATUS(status);
+        }
+        else {
+            return ERROR_CODE;
+        }
+    }
+    else {
+        return ERROR_CODE;
+    }
+
+
     // remember to return ERROR_CODE on error.
-    return ERROR_CODE;
 }
