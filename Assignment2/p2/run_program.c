930b4b8c5cb414deb6a92600a8507af5f796392a
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-01-22 00:15:29 +0000

    Update run_program.c

--- Assignment2/p2/run_program.c
+++ Assignment2/p2/run_program.c
@@ -1,9 +1,9 @@
 #include "run_program.h"
-#define _GNU_SOURCE
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 /*remember to check out https://man7.org/linux/man-pages/man3/exec.3.html*/
 
@@ -11,19 +11,41 @@
 
 int run_program(char *file_path, char *argv[])
 {
-    int command = fork();
+    pid_t = fork();
+    int status;
 
     if (file_path == NULL) {
         return ERROR_CODE;
     }
 
-    else {
-        execvp(file_path, argv);
+    if (pid == 0) {
+        if (argv == NULL) {
+            execlp(file_path, file_path, NULL);
+        }
+        
+        else {
+            execvp(file_path, argv);
+        }
+        exit(ERROR_CODE);
     }
 
-    return ERROR_CODE;
-
-
+    else if (pid > 0) {
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status)) {
+            return WEXITSTATUS(status);
+        }
+        else {
+            return ERROR_CODE
+        }
+    }
 
     // remember to return ERROR_CODE on error.
+    else {
+        return ERROR_CODE;
+    }
+    /*return ERROR_CODE;*/
+
+
+
+
 }
