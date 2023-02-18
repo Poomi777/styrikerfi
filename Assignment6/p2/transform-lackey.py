1cd7d71a04e873ebb689319a7ea0df4921bffd7f
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-02-18 22:56:51 +0000

    Add files via upload

---
+++ Assignment6/p2/transform-lackey.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Put your script here (instead of the print statements)
+# Your script shall read the valgrind output on stdin,
+# and produce a list of newline-separated lines of virtual page numbers
+
+import sys
+
+for line in sys.stdin:
+    print(42)
