f75e15577e60e3def59b57d1693552656d7e7e6a
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-03-06 12:25:25 +0000

    Add files via upload

---
+++ Assignment8/p1/main.c
@@ -0,0 +1,36 @@
+#include "testlib.h"
+#include "pipe.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    test_start("pipe.c");
+
+    printf("Running ls\n");
+    errno = 0;
+    char *argv1[] = {
+        "/bin/ls",
+        "/home",
+        NULL
+    };
+    int result1 = run_program(argv1);
+    test_equals_int(result1, 0);
+    test_equals_int(errno, 0);
+
+    printf("Running unknown-program-name\n");
+    errno = 0;
+    char * argv2[] = {
+	"unknown-program-name",
+        NULL
+    };
+    int result2 = run_program(argv2);
+    test_equals_int(result2, -1);
+    test_equals_int(errno, ENOENT);
+    printf("Error: %s (errno=%d)\n", strerror(errno), errno);
+
+    return test_end();
+}
---
+++ Assignment8/p1/pipe.c
@@ -0,0 +1,87 @@
+// This changes the way some includes behave.
+// This should stay before any include.
+#define _GNU_SOURCE
+
+#include "pipe.h"
+#include <sys/wait.h> /* For waitpid */
+#include <unistd.h> /* For fork, pipe */
+#include <stdlib.h> /* For exit */
+#include <fcntl.h>
+#include <errno.h>
+
+int run_program(char *argv[])
+{
+
+    if (argv == NULL) {
+        return -1;
+    }
+
+    // -------------------------
+    // TODO: Open a pipe
+    // -------------------------
+    int pipe_fileDescript[2];
+    
+    if (pipe2(pipe_fileDescript, O_CLOEXEC) == -1) {
+        return -1;
+    }
+
+    int child_pid = fork();
+    if (child_pid == -1) {
+        close(pipe_fileDescript[0]);
+        close(pipe_fileDescript[1]);
+        return -1
+    } 
+    
+    else if (child_pid == 0) {
+        close(pipe_fileDescript[0]);
+
+        // Replace program
+        execvp(argv[0], argv);
+
+        // -------------------------
+        // TODO: Write the error on the pipe
+        // -------------------------
+        int exec_errno = errno;
+        write(pipe_fileDescript[1], &exec_errno, sizeof(exec_errno));
+
+
+        exit(0);
+    } 
+    
+    else {
+        close(pipe_fds[1]);
+
+        int status, hadError = 0;
+
+        int waitError = waitpid(child_pid, &status, 0);
+        if (waitError == -1) {
+            // Error while waiting for child.
+            hadError = 1;
+        } 
+        
+        else if (!WIFEXITED(status)) {
+            // Our child exited with another problem (e.g., a segmentation fault)
+            // We use the error code ECANCELED to signal this.
+            hadError = 1;
+            errno = ECANCELED;
+        } 
+        
+        else {
+            // -------------------------
+            // TODO: If there was an execvp error in the child, set errno
+            //       to the value execvp set it to.
+            // -------------------------
+
+            int child_errno;
+            
+            if (read(pipe_fileDescript[0], &child_errno, sizeof(child_errno)) == sizeof(child_errno)) {
+                errno = child_errno;
+                hadError = 1;
+            }
+        }
+        close(pipe_fileDescript[0]);
+
+
+        return hadError ? -1 : WEXITSTATUS(status);
+    }
+}
