c54b3659afb33c23c51e947aff6658b1eed08a03
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-03-12 19:38:53 +0000

    Update multi_mutex.c

--- Assignment9/p2/multi_mutex.c
+++ Assignment9/p2/multi_mutex.c
@@ -21,6 +21,8 @@ int multi_mutex_unlock(pthread_mutex_t **mutexv)
         }
         i++;
     }
+
+    return 0;
 }
 
 int multi_mutex_trylock(pthread_mutex_t **mutexv)
@@ -46,6 +48,8 @@ int multi_mutex_trylock(pthread_mutex_t **mutexv)
     if (!success) {
         return -1;
     }
+
+    return 0;
 }
 
 // Just for optional bonus points
@@ -111,5 +115,7 @@ int multi_mutex_lock(pthread_mutex_t **mutexv)
 
         return -1;
     }
+
+    return 0;
 }
 
