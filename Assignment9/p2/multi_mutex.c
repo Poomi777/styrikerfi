593e920b169c790e269904971dd2e9366327c520
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-03-12 19:21:22 +0000

    Update multi_mutex.c

--- Assignment9/p2/multi_mutex.c
+++ Assignment9/p2/multi_mutex.c
@@ -7,14 +7,45 @@
 
 int multi_mutex_unlock(pthread_mutex_t **mutexv)
 {
-    (void) mutexv;
-    return 0;
+    if (mutexv == NULL) {
+        return -1;
+    }
+
+    int i = 0;
+
+    while (mutexv[i] != NULL) {
+        int returner = pthread_mutex_unlock(mutexv[i]);
+
+        if (returner != 0) {
+            return -1
+        }
+        i++;
+    }
 }
 
 int multi_mutex_trylock(pthread_mutex_t **mutexv)
 {
-    (void) mutexv;
-    return 0;
+    if (mutexv == NULL) {
+        return -1;
+    }
+
+    int i = 0;
+    int success = 1;
+
+    while (mutexv[i] != NULL && success) {
+        int returner = pthread_mutex_trylock(mutexv[i]);
+
+        if (returner != 0) {
+            multi_mutex_unlock(mutexv);
+            success = 0;
+        }
+
+        i++;
+    }
+
+    if (!success) {
+        return -1;
+    }
 }
 
 // Just for optional bonus points
@@ -22,7 +53,35 @@ int multi_mutex_trylock(pthread_mutex_t **mutexv)
 // i.e. no mutex may be held while blocking
 int multi_mutex_lock(pthread_mutex_t **mutexv)
 {
-    (void) mutexv;
-    return 0;
+    if (mutexv == NULL) {
+        return -1;
+    }
+
+    int i = 0;
+    int acquired = 0;
+    int backoff = 1
+
+    struct timespec ts;
+
+    while (mutexv[i] != NULL) {
+        int returner = pthread_mutex_trylock(mutexv[i]);
+
+        if (returner != 0) {
+            multi_mutex_unlock(mutexv, i);
+            acquired = 0;
+
+
+            ts.tv_sec = 0;
+            ts.tv_nsec = (backoff * (rand() % MAX_BACKOFF_MS)) * 1000000;
+            nanosleep(&ts, NULL);
+
+            backoff = (backoff < MAX_BACKOFF_MS) ? (backoff * 2) : MAX_BACKOFF_MS;
+        }
+
+        else {
+            i++;
+            acquired++;
+        }
+    }
 }
 
