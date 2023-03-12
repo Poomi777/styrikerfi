7133ad5cd57edf7190a701311eaee1bd0b1c33c3
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-03-12 19:36:01 +0000

    Update multi_mutex.c

--- Assignment9/p2/multi_mutex.c
+++ Assignment9/p2/multi_mutex.c
@@ -17,7 +17,7 @@ int multi_mutex_unlock(pthread_mutex_t **mutexv)
         int returner = pthread_mutex_unlock(mutexv[i]);
 
         if (returner != 0) {
-            return -1;
+            return -1
         }
         i++;
     }
@@ -30,18 +30,21 @@ int multi_mutex_trylock(pthread_mutex_t **mutexv)
     }
 
     int i = 0;
-    int acquired = 0;
+    int success = 1;
 
-    while (mutexv[i] != NULL) {
+    while (mutexv[i] != NULL && success) {
         int returner = pthread_mutex_trylock(mutexv[i]);
 
         if (returner != 0) {
-            multi_mutex_unlock(mutexv, i);
-            return -1;
+            multi_mutex_unlock(mutexv);
+            success = 0;
         }
 
         i++;
-        acquired++;
+    }
+
+    if (!success) {
+        return -1;
     }
 }
 
@@ -55,30 +58,58 @@ int multi_mutex_lock(pthread_mutex_t **mutexv)
     }
 
     int i = 0;
-    int acquired = 0;
-    int backoff = 1;
+    int number_of_mutexes = 0;
+    int success = 1;
 
-    struct timespec ts;
+    while (mutexv[number_of_mutexes] != NULL) {
+        number_of_mutexes++;
+    }
 
-    while (mutexv[i] != NULL) {
-        int returner = pthread_mutex_trylock(mutexv[i]);
+    while (i < number_of_mutexes && success) {
+        if (i > 0) {
+            int j = i-1;
+
+            while (j >= 0) {
+                int returner = pthread_mutex_unlock(mutexv[j]);
+
+                if (returner != 0) {
+                    multi_mutex_unlock(mutexv);
+                    success = 0;
+                    break;
+                }
+
+                j--;
+            }
+        }
+
+        if (!success) {
+            break;
+        }
+
+        int returner = pthread_mutex_lock(mutexv[i]);
 
         if (returner != 0) {
-            multi_mutex_unlock(mutexv, i);
-            acquired = 0;
-
-
-            ts.tv_sec = 0;
-            ts.tv_nsec = (backoff * (rand() % MAX_BACKOFF_MS)) * 1000000;
-            nanosleep(&ts, NULL);
-
-            backoff = (backoff < MAX_BACKOFF_MS) ? (backoff * 2) : MAX_BACKOFF_MS;
+            success = 0;
         }
 
-        else {
-            i++;
-            acquired++;
+        i++;
+    }
+
+    if (!success) {
+
+        int j = i-1;
+
+        while (j >= 0) {
+            int returner = pthread_mutex_unlock(mutexv[j]);
+
+            if (returner != 0) {
+                return -1;
+            }
+
+            j--;
         }
+
+        return -1;
     }
 }
 
