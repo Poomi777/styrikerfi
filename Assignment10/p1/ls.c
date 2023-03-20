b3246a15b08670496270d255380f5a405abd7886
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-03-20 13:20:04 +0000

    Update ls.c

--- Assignment10/p1/ls.c
+++ Assignment10/p1/ls.c
@@ -4,133 +4,64 @@
  * Student 2 name: xx 
  */
 
-#define _POSIX_C_SOURCE 2
-#include "copy.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <fcntl.h>
+#include "ls.h"
+// You may not need all these headers ...
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <dirent.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-// No need to change this. Parses argc into the CopyArgs structure
-int parseCopyArgs(int argc, char * const argv[], CopyArgs* args)
+// Use this function to display the files. DO NOT CHANGE IT.
+void _printLine(unsigned int size, unsigned int sizeOnDisk, const char* name)
 {
-    if (args == NULL) {
-        return -1;
-    }
-
-    // Initialize with default values
-    args->blocksize  = 1024;
-
-    int opt;
-    while ((opt = getopt(argc, argv, "b:")) != -1) {
-        switch (opt) {
-            case 'b':
-                args->blocksize = atoi(optarg);
-
-                if ((errno != 0) || (args->blocksize <= 0)) {
-                    return -1; // Invalid blocksize argument.
-                }
-
-                break;
-
-            default: /* '?' */
-                return -1;
-        }
-    }
-
-    if (optind != argc - 2) {
-        return -1; // We expect two parameters after the options.
-    }
-
-    args->from = argv[optind];
-    args->to   = argv[optind + 1];
-
-    return 0;
+    printf("%10u   %10u   %s\n", size, sizeOnDisk, name);
 }
 
+// Assume this to be the maximum length of a file name returned by readdir
+#define MAX_FILE_NAME_LENGTH 255
 
-int doCopy(CopyArgs* args)
+int list(const char* path, const char *suffixFilter)
 {
-    if (args == NULL) {
+    DIR *directory = opendir(path);
+
+    if (directory == NULL) {
+        perror("opendir failed");
         return -1;
     }
 
-    // ----------------
-    // Copy the file.
-    // ----------------
+    struct dirent *entry;
 
-    int source_file = open(args->from, O_RDONLY);
+    while ((entry = readdir(directory)) != NULL) {
+        if (entry->d_name[0] == '.') {
+            continue;
+        }
 
-    if (source_file == -1) {
-        perror("Failed to open source file")
-        return -1;
-    }
-
-    struct stat st;
-    if (stat(args->to, %st) == 0) {
-        fprintf(stderr, "Destination file already exists\n");
-        close(source_file);
-        return -1;
-    }
-
-    int destination_file = open(args->to, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR);
-
-    if (destination_file == -1) {
-        perror("Failed to create destination file");
-        close(source_file);
-        return -1;
-    }
-
-    char buffer[args->blocksize];
-    int bytes_read;
-
-    while ((bytes_read = read(source_file, buffer, args->blocksize)) > 0) {
-        int zeroes = 1;
-
-        for (int i = 0; i < bytes_read; i++) {
-            if (buffer[i] != 0) {
-                zeroes = 0;
-                break;
+        if (suffixFilter != NULL && strcmp(suffixFilter, "") != 0) {
+            char *suffix = strrchr(entry->d_name, '.');
+            if (suffix == NULL || strcmp(suffix, suffixFilter) != 0) {
+                continue;
             }
         }
 
-        if (zeroes) {
-            if (lseek(destination_file, bytes_read, SEEK_CUR) == -1) {
-                perror("Failed to seek destination file");
-                close(source_file);
-                close(destination_file)
-                return -1;
-            }
+        char full_path[MAX_FILE_NAME_LENGTH + strlen(path) + 2];
+        snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+
+        struct stat st;
+        
+        if (stat(full_path, &st) == -1) {
+            perror("failed to stat");
+            
+            closedir(directory);
+            return -1;
         }
 
-        else {
-            if (write(destination_file, buffer, bytes_read) == -1) {
-                perror("Failed to write to destination file");
-                close(source_file);
-                close(destination_file);
-                return -1
-            }
-        }
+        _printLine(st.st_size, st.st_blocks * 512, entry->d_name);
     }
 
-    if (bytes_read == -1) {
-        perror("Failed to read source file");
-        close(source_file);
-        close(destination_file);
-        return -1;
-    }
-
-
-    if (fchmod(destination_file, S_IRUSR) == -1) {
-        perror("Failed to set permissions on destination file");
-        close(source_file);
-        close(destination_file);
-        return -1;
-    }
-
-    close(source_file);
-    close(destination_file);
+    closedir(directory);
     return 0;
 }
