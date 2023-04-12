f7b2be89e726210873bc7a56b3b585ffdf54195d
Poomi777 <70536312+Poomi777@users.noreply.github.com>
2023-04-12 10:56:58 +0000

    Update main.cpp

--- cpp_finalexam/B3/main.cpp
+++ cpp_finalexam/B3/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include "../B2/car_dealer.h"
 
 using namespace std;
 
 // Implement read_results_from_file(std::string filename) here
+
+vector<Order> read_results_from_file(string filename) {
+    ifstream input_file(filename);
+    vector<Order> orders;
+
+    if (input_file.is_open()) {
+        string buyer, manufacturer, series;
+        string engine, price_str, received_str;
+        double price;
+        bool received;
+
+        while (input_file >> buyer >> manufacturer >> series >> engine >> price_str >> received_str) {
+            price = stod(price_str);
+            received = (received_str == "1");
+            orders.emplace_back(buyer, manufacturer, series, engine, price, received);
+        }
+
+        input_file.close();
+    }
+
+    return orders;
+}
+
 
 int main(){
     auto results = read_results_from_file("result_test_file.txt");
